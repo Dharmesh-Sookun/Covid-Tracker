1208e16952329ee6c01f26d9546060d2b7be00b3
Dharmesh-Sookun <sookundharmesh2906@gmail.com>
2021-09-30 19:47:28 +0400

    Added loading spinner

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,35 @@
+const mainContainer = document.querySelector(".container");
+const loader = document.querySelector("#loader");
 const date = document.querySelector("#date");
 const confirmed = document.querySelector("#confirmed");
 const death = document.querySelector("#deaths");
 const recovered = document.querySelector("#recovered");
 
-function getData(){
-    const API_ENDPOINT = "https://pomber.github.io/covid19/timeseries.json";
-    fetch(API_ENDPOINT)
+mainContainer.style.display = "none";
+
+function getData() {
+  const API_ENDPOINT = "https://pomber.github.io/covid19/timeseries.json";
+  fetch(API_ENDPOINT)
     .then((response) => response.json())
-    .then((json)=> processData(json))
-    .catch(err => console.error(err));
+    .then((json) => processData(json))
+    .catch((err) => console.error(err));
 }
 
-function processData(data){
-    //console.log(extractMauritiusData(data));
-    const latest = extractMauritiusData(data);
-    date.innerHTML = latest.date;
-    confirmed.innerHTML = `Confimed: ${latest.confirmed}`;
-    death.innerHTML = `Death:${latest.deaths}`;
-    recovered.innerHTML = `Recovered: ${latest.recovered}`; 
+function processData(data) {
+  //console.log(extractMauritiusData(data));
+  const latest = extractMauritiusData(data);
+  date.innerHTML = latest.date;
+  confirmed.innerHTML = `Confimed: ${latest.confirmed}`;
+  death.innerHTML = `Death:${latest.deaths}`;
+  recovered.innerHTML = `Recovered: ${latest.recovered}`;
+  setTimeout(() => {
+    loader.style.display = "none";
+    mainContainer.style.display = "flex";
+  }, 2000);
 }
 
-function extractMauritiusData(data){
-    return data.Mauritius[data.Mauritius.length - 1];
+function extractMauritiusData(data) {
+  return data.Mauritius[data.Mauritius.length - 1];
 }
 
 getData();
